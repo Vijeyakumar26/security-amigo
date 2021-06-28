13d4078501fd726ed751c0ab3242700c34c5f19d
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-06-28 15:32:00 +0530

    Created Student Bean

---
+++ student-security/src/main/java/com/security/entity/Student.java
@@ -0,0 +1,64 @@
+package com.security.entity;
+
+public class Student {
+	
+	private Integer studentId;
+	private String studentName;
+	
+	public Student() {
+		super();
+	}
+	public Student(Integer studentId, String studentName) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+	}
+	public Integer getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((studentId == null) ? 0 : studentId.hashCode());
+		result = prime * result + ((studentName == null) ? 0 : studentName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (studentId == null) {
+			if (other.studentId != null)
+				return false;
+		} else if (!studentId.equals(other.studentId))
+			return false;
+		if (studentName == null) {
+			if (other.studentName != null)
+				return false;
+		} else if (!studentName.equals(other.studentName))
+			return false;
+		return true;
+	}
+	
+	
+
+}
